84fa460880e40f8065fc6fc4e897459a600ce656
Kene <kayyungx@gmail.com>
2022-11-15 11:34:53 +0100

    Fetch rockets data

--- src/components/RocketsPage.js
+++ src/components/RocketsPage.js
@@ -1,5 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector, shallowEqual, useDispatch } from 'react-redux';
+import { fetchRockets } from '../redux/rocketAction';
 
-const RocketPage = () => <h3>Rockets</h3>;
+const RocketPage = () => {
+  const apiRockets = useSelector((state) => state.rocket, shallowEqual);
+  console.log(apiRockets);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchRockets());
+  }, [dispatch]);
+
+  return <h3>Rockets</h3>;
+};
 
 export default RocketPage;

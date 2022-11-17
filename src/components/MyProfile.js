28edbd6574fde7e2297e0d204035d3654302c312
gandradep <eventsonido@gmail.com>
2022-11-17 11:44:57 +0100

    Display Mission

--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -4,12 +4,15 @@ import { useSelector } from 'react-redux';
 const MyProfile = () => {
   const missions = useSelector((state) => state.missions);
   const joinMission = missions.filter((item) => item.reserved === true);
-  console.log(joinMission);
   return (
     <div>
-      <div>
+      <div className="joinMission">
         <h3>My Missions</h3>
-        
+        <ul>
+          {joinMission?.map((mission) => (
+            <li key={mission.id}>{mission.name}</li>
+          ))}
+        </ul>
       </div>
     </div>
   );

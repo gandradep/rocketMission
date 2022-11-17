ede297dc98fd786bdad0d3b8d6e1adc61c4538f4
gandradep <eventsonido@gmail.com>
2022-11-17 11:37:12 +0100

    Get reserved missions array

--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
-const MyProfile = () => (
-  <div>
-    <p>Test</p>
-  </div>
-);
+const MyProfile = () => {
+  const missions = useSelector((state) => state.missions);
+  const joinMission = missions.filter((item) => item.reserved === true);
+  console.log(joinMission);
+  return (
+    <div>
+      <div>
+        <h3>My Missions</h3>
+        
+      </div>
+    </div>
+  );
+};
 
 export default MyProfile;

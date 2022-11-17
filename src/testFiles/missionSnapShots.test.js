3b5903e36e4f129833bb7bf79010732f3ea6299e
gandradep <eventsonido@gmail.com>
2022-11-17 14:26:32 +0100

    Fix linters

--- src/testFiles/missionSnapShots.test.js
+++ src/testFiles/missionSnapShots.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { render } from "@testing-library/react";
+import { render } from '@testing-library/react';
 import { Provider } from 'react-redux';
-import  store from '../redux/store';
+import store from '../redux/store';
 import App from '../App';
 
 describe('Appcomponents snapshots', () => {
@@ -9,7 +9,7 @@ describe('Appcomponents snapshots', () => {
     const tree = render(
       <Provider store={store}>
         <App />
-      </Provider>
+      </Provider>,
     );
     expect(tree).toMatchSnapshot();
   });
